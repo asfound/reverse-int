aafd18d7491cece09e0a13faf8ee857b02c4ba44
asfound <fffound27@gmail.com>
2024-08-07 13:13:14 +0200

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+  return Math.abs(n).toString().split('').reverse().join('')
 }
+ console.log(Number(Math.abs(250).toString().split('').reverse().join('')))
